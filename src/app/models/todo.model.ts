26572bbc0f3102f5f732725a86398c7ee75964c0
Batr02 <heorhiibatrynu@gmail.com>
2026-05-15 00:17:30 +0300

    add todo model interfaces

---
+++ src/app/models/todo.model.ts
@@ -0,0 +1,14 @@
+export interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+  userId: number;
+}
+
+export interface CreateTodoDto {
+  title: string;
+  completed: boolean;
+  userId: number;
+}
+
+export type TodoFilter = 'all' | 'active' | 'completed';
