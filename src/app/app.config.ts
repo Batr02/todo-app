7eb19d7a82e2ef7f7abf4a797d21c83a7bcd2c95
Batr02 <heorhiibatrynu@gmail.com>
2026-05-15 00:36:21 +0300

    add http and animation providers

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,11 @@
+import { provideHttpClient } from '@angular/common/http';
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    
+    provideHttpClient(),
+    provideAnimationsAsync(),
   ]
 };
