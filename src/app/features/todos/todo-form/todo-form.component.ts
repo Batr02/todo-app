a9800361cbfa0d024aa8d016b17ee0ff34ff7358
Batr02 <heorhiibatrynu@gmail.com>
2026-05-15 01:48:37 +0300

    implement todo form component

---
+++ src/app/features/todos/todo-form/todo-form.component.ts
@@ -0,0 +1,35 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+
+@Component({
+  selector: 'app-todo-form',
+  imports: [
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+  ],
+  templateUrl: './todo-form.component.html',
+  styleUrl: './todo-form.scss',
+})
+export class TodoFormComponent {
+  @Output() addTodo = new EventEmitter<string>();
+
+  titleControl = new FormControl('', [
+    Validators.required,
+    Validators.minLength(3),
+    Validators.maxLength(100),
+  ]);
+
+  submit(): void {
+    if (this.titleControl.valid && this.titleControl.value) {
+      this.addTodo.emit(this.titleControl.value.trim());
+      this.titleControl.reset();
+    }
+  }
+}
