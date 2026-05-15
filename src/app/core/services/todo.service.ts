aae3db888f5a697d8e1e5491ee9dc05afe57a1f7
Batr02 <heorhiibatrynu@gmail.com>
2026-05-15 15:38:47 +0300

    delete loadTodos logic

--- src/app/core/services/todo.service.ts
+++ src/app/core/services/todo.service.ts
@@ -16,22 +16,6 @@ export class TodoService {
   private todosSubject = new BehaviorSubject<Todo[]>([]);
   todos$ = this.todosSubject.asObservable();
 
-  private loadingSubject = new BehaviorSubject<boolean>(false);
-  loading$ = this.loadingSubject.asObservable();
-
-  loadTodos(): void {
-    this.loadingSubject.next(true);
-    this.http
-      .get<Todo[]>(`${this.API}?userId=${this.USER_ID}&_limit=10`)
-      .subscribe({
-        next: (todos) => {
-          this.todosSubject.next(todos);
-          this.loadingSubject.next(false);
-        },
-        error: () => this.loadingSubject.next(false),
-      });
-  }
-
   addTodo(title: string): Observable<Todo> { 
     const dto: CreateTodoDto = {
       title,
