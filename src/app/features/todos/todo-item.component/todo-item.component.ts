bf3ce94c9c954c60044927c0a752df6ec7b51211
Batr02 <heorhiibatrynu@gmail.com>
2026-05-15 17:10:19 +0300

    impement logic of the edit mode

--- src/app/features/todos/todo-item.component/todo-item.component.ts
+++ src/app/features/todos/todo-item.component/todo-item.component.ts
@@ -4,6 +4,9 @@ import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { Todo } from '../../../models/todo.model';
+import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @Component({
   selector: 'app-todo-item',
@@ -12,6 +15,9 @@ import { Todo } from '../../../models/todo.model';
     MatButtonModule,
     MatIconModule,
     MatTooltipModule,
+    MatInputModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
   ],
   templateUrl: './todo-item.component.html',
   styleUrl: './todo-item.component.scss',
@@ -21,4 +27,30 @@ export class TodoItemComponent {
   @Input() loading = false;
   @Output() toggle = new EventEmitter<Todo>();
   @Output() delete = new EventEmitter<number>();
+  @Output() edit = new EventEmitter<{ todo: Todo; newTitle: string }>();
+
+  isEditing = false;
+  editControl = new FormControl('', [
+    Validators.required,
+    Validators.minLength(3),
+    Validators.maxLength(100),
+  ]);
+
+  startEdit(): void {
+    this.isEditing = true;
+    this.editControl.setValue(this.todo.title);
+  }
+
+  cancelEdit(): void {
+    this.isEditing = false;
+    this.editControl.reset();
+  }
+
+  confirmEdit(): void {
+    if (this.editControl.valid && this.editControl.value) {
+      this.edit.emit({ todo: this.todo, newTitle: this.editControl.value.trim() });
+      this.isEditing = false;
+    }
+  }
+
 }
